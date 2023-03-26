4ed52cf53df8cd5427667b197755a4eaaa049470
AbhayKachrola <abhay.kachrola112820@marwadiuniversity.ac.in>
2023-03-26 10:36:05 +0530

    new commit

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,70 +1,20 @@
+// __tests__/todo.js
 /* eslint-disable no-undef */
-const todoList = require("../todo");
+const db = require("../models");
 
-const {
-  all,
-  markAsComplete,
-  add,
-  overdue,
-  dueToday,
-  dueLater,
-  today,
-  tomorrow,
-  yesterday,
-} = todoList();
+describe("Todolist Test Suite", () => {
+  beforeAll(async () => {
+    await db.sequelize.sync({ force: true });
+  });
 
-describe("ToDo test Suite", () => {
-  beforeAll(() => {
-    add({
-      title: "Submit assignment",
-      dueDate: yesterday,
+  test("Should add new todo", async () => {
+    const todoItemsCount = await db.Todo.count();
+    await db.Todo.addTask({
+      title: "Test todo",
       completed: false,
+      dueDate: new Date(),
     });
-    add({
-      title: "Pay rent",
-      dueDate: today,
-      completed: true,
-    });
-  });
-  test("Should add new todo", () => {
-    let count = all.length;
-    add({
-      title: "new todo",
-      completed: false,
-      dueDate: new Date().toLocaleDateString("en-IN"),
-    });
-    expect(all.length).toBe(count + 1);
-  });
-  test("Should mark a todo as complete", () => {
-    expect(all[0].completed).toBe(false);
-    markAsComplete(0);
-    expect(all[0].completed).toBe(true);
-  });
-  test("Should Retrieves overdue items", () => {
-    let count = overdue().length;
-    add({
-      title: "Pay electric bill",
-      dueDate: yesterday,
-      completed: false,
-    });
-    expect(overdue().length).toBe(count + 1);
-  });
-  test("Should Retrieves due today items", () => {
-    let count = dueToday().length;
-    add({
-      title: "Pay electric bill",
-      dueDate: today,
-      completed: false,
-    });
-    expect(dueToday().length).toBe(count + 1);
-  });
-  test("Should Retrieves due later items", () => {
-    let count = dueLater().length;
-    add({
-      title: "Pay electric bill",
-      dueDate: tomorrow,
-      completed: false,
-    });
-    expect(dueLater().length).toBe(count + 1);
+    const newTodoItemsCount = await db.Todo.count();
+    expect(newTodoItemsCount).toBe(todoItemsCount + 1);
   });
 });
